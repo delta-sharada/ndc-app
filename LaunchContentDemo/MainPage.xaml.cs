f5d5e25e55ef2cd9e8c79d1102e943fc6c2aa625
Sharada <ms.sharada@gmail.com>
2018-05-29 23:01:59 -0400

    First draft of ndc app
    Contains microsite integration through custom tabs/safaricontroller

---
+++ LaunchContentDemo/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LaunchContentDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            // We rely on the built-in service lcoator in this example, but you could just
+            // as easily locate this service using DI and launch from your ViewModel
+            var service = DependencyService.Get<INativeBrowserService>();
+            {
+                if (service == null) return;
+
+                service.LaunchNativeEmbeddedBrowser("https://delta-sharada.github.io/ndc-content/");
+            }
+        }
+    }
+}
